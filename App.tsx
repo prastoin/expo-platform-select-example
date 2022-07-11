e3386367b44c5b689dee7955800a8d95be8e7ebc
prastoin <tkesray@gmail.com>
2022-07-11 15:22:27 +0200

    feat: dynamic rendering depending on platform

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
 import { StatusBar } from "expo-status-bar";
+import GoogleMapReact from "google-map-react";
 import * as React from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Platform, StyleSheet, View } from "react-native";
 import MapView from "react-native-maps";
 
 export default function App() {
+  const platformIsWeb = Platform.OS === "web";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <MapView style={styles.map} />
+      {platformIsWeb ? (
+        <div style={{ height: "100vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "dev" }}
+            defaultCenter={{ lat: 0, lng: 0 }}
+            defaultZoom={1}
+          ></GoogleMapReact>
+        </div>
+      ) : (
+        <MapView style={styles.map} />
+      )}
     </View>
   );
 }
