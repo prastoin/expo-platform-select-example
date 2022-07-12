d4970a4df607c367f485d86b2f8925b7996c425b
prastoin <tkesray@gmail.com>
2022-07-12 09:52:15 +0200

    refactor: app.tsx to be smaller

--- App.tsx
+++ App.tsx
@@ -1,31 +1,22 @@
-import { StatusBar } from "expo-status-bar";
 import * as React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native";
 import MapComponent from "./MapComponent";
 
 export default function App() {
+  const defaultMapCenterCoords = {
+    lat: 48.866667,
+    lng: 2.333333,
+  };
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <MapComponent
-        positionConstraintPosition={{
-          lat: 48.866667,
-          lng: 2.333333,
-        }}
-      />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <MapComponent defaultMapCenterCoords={defaultMapCenterCoords} />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-});
